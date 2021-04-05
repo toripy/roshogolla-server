31e7f2ebb5ba8f06da81e2964a2779e6650c5fae
toriqul <toriqul.stu@gmail.com>
2021-04-05 19:07:32 +0600

    last commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require( 'express' )
 const bodyParser = require( 'body-parser' );
 var cors = require( 'cors' );
 const MongoClient = require( 'mongodb' ).MongoClient;
-const ObjectID = require( 'mongodb' ).ObjectID()
+const ObjectId = require( 'mongodb' ).ObjectID
 
 const port = process.env.PORT;
 const app = express()
@@ -24,14 +24,15 @@ app.get( '/', ( req, res ) => {
 client.connect( err => {
 
   const sweetCollection = client.db( "roshogolla" ).collection( "products" );
-
+  const ordersCollection = client.db( "roshogolla" ).collection( "orders" );
+  const PlacedOrdersCollection = client.db( "roshogolla" ).collection( "placedOrder" );
   app.post( '/addProduct', ( req, res ) => {
     const newProduct = req.body;
     console.log( newProduct );
     sweetCollection.insertOne( newProduct )
       .then( result => {
         res.send( result )
-        
+
       } )
   } )
 
@@ -39,9 +40,68 @@ client.connect( err => {
     sweetCollection.find( {} )
       .toArray( ( err, docs ) => {
         res.send( docs )
-        console.log(docs)
+
       } )
   } )
+  app.post( '/orders', ( req, res ) => {
+    const order = req.body.key
+    sweetCollection.find( { "_id": ObjectId( order ) } )
+      .toArray( ( err, data ) => {
+
+        const { _id } = data[0]
+        ordersCollection.find( { "_id": ObjectId( _id ) } )
+          .toArray( ( err, docs ) => {
+
+
+            if ( docs.length === 0 ) {
+              data[0].quantity = 1
+               data[0].date=req.body.date
+                data[0].user= req.body.user
+                data[0].email= req.body.email
+                
+             
+              ordersCollection.insertOne( data[0] )
+            }
+            else {
+
+              ordersCollection.updateOne( { '_id': ObjectId( docs[0]._id ) }, {
+                $inc: { quantity: 1 }
+
+              } )
+            }
+          } )
+      } )
+
+  } )
+
+    app.get( '/orderedProduct', ( req, res ) => {
+    
+    ordersCollection.find( {} )
+      .toArray( ( err, docs ) => {
+        res.send( docs )
+
+      } )
+  } )
+   app.delete('/deleteAddedProduct', ( req, res ) => {
+        ordersCollection.deleteMany({})
+        
+    })
+
+    app.post('/ordered',(req,res)=>{
+    PlacedOrdersCollection.insertOne( req.body )
+            .then( result => {
+
+                res.send( result )
+            } )
+    
+    
+})
+    app.delete('/delete/:key',(req,res)=>{
+      sweetCollection.deleteOne({'_id': ObjectId(req.params.key)})
+})
+    
+
+
 
 } );
 
@@ -49,4 +109,5 @@ client.connect( err => {
 
 
 
-app.listen( port || 5000 )
+
+app.listen( 5001 )
